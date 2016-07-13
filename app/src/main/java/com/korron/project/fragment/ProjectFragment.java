f567124f77e8d5f1d7aa3222c88d2ab1181a0876
xxz1990 <592244674@qqcom>
2016-07-13 17:31:51 +0800

    2016-07-13 17:31:41
    2016-07-13 17:31:45

--- app/src/main/java/com/korron/project/fragment/ProjectFragment.java
+++ app/src/main/java/com/korron/project/fragment/ProjectFragment.java
@@ -6,16 +6,32 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.korron.project.R;
 
 /**
  * Created by milli on 2016/7/13.
  */
 public class ProjectFragment extends Fragment {
 
+    private TextView mTvHangYe;
+    private TextView mTvXuQiu;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
 
+        View view = inflater.inflate(R.layout.fragment_project,null);
+
+        initView(view);
+
+        return view;
+
+    }
+
+    private void initView(View view) {
+        mTvHangYe = (TextView) view.findViewById(R.id.tv_hangye);
+        mTvXuQiu = (TextView) view.findViewById(R.id.tv_xuqiu);
     }
 }
